b3c801a65df3fadfcd01ff797690b1e4363bd6c6
Edwin Nyawoli <edwin.nyawoli@affinitylabsafrica.com>
2021-01-16 19:15:09 +0000

    Add provider for CurrencyService

--- lib/app.dart
+++ lib/app.dart
@@ -1,18 +1,28 @@
+import 'package:currency_picker/currency_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:opencc/theme.dart';
 import 'package:opencc/ui/home.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
 
 class OpenCCApp extends StatelessWidget {
   static const String initialRoute = '/';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: getTheme(context),
-      initialRoute: initialRoute,
-      routes: <String, WidgetBuilder>{
-        initialRoute: (BuildContext context) => HomePage()
-      },
+    final CurrencyService currencyService = CurrencyService();
+
+    return MultiProvider(
+      providers: <SingleChildWidget>[
+        Provider<CurrencyService>.value(value: currencyService),
+      ],
+      child: MaterialApp(
+        theme: getTheme(context),
+        initialRoute: initialRoute,
+        routes: <String, WidgetBuilder>{
+          initialRoute: (BuildContext context) => HomePage()
+        },
+      ),
     );
   }
 }
